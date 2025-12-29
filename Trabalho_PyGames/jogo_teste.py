6b2cf6c9ed49d3832db5b62198f46064140566f6
Eduardo23Ribeiro <eduardobr728@gmail.com>
2025-12-29 18:19:52 -0300

    Subindo de novo pq não subiu  certo aff

--- Trabalho_PyGames/jogo_teste.py
+++ Trabalho_PyGames/jogo_teste.py
@@ -1,12 +1,20 @@
 import pygame
 
-# att 1
+# att 2
 pygame.init()
 
 botao_menu_teste = pygame.Rect(500, 350, 200, 100)
 cor_botao_menu_teste = (255, 0, 0)
 tela = pygame.display.set_mode((1200, 800))
+clock = pygame.time.Clock()
 rodando = True
+
+
+imagem = pygame.image.load("Trabalho_PyGames/Gemini_Generated_Image_pgro94pgro94pgro.png")
+imagem = imagem.convert()
+color = imagem.get_at((0,0))
+imagem.set_colorkey(color)
+
 
 while rodando:
     for evento in pygame.event.get():
@@ -15,7 +23,7 @@ while rodando:
     if evento.type == pygame.MOUSEBUTTONDOWN:
          if botao_menu_teste.collidepoint(evento.pos): # o collidepoint verifica se o mouse está sobre o botão.
              #ele tbm evita ter que ficar fazendo conta da posição do mouse :) 
-             print("botão teste foi clicado iniciar o game")
+             print("botão teste foi clicado , iniciar o game")
 
     mouse_pos = pygame.mouse.get_pos()
 
@@ -25,11 +33,12 @@ while rodando:
         cor_botao_menu_teste = (255, 0, 0)
 
 
-    tela.fill((0, 0, 0))
+    tela.fill((200, 200, 200))
+    tela.blit(imagem, (0,0))
     pygame.draw.rect(tela, cor_botao_menu_teste, botao_menu_teste)
     pygame.display.update()
 
-    pygame.time.Clock().tick(60)
+    clock.tick(60)
 
 
 
@@ -40,8 +49,4 @@ while rodando:
 
 
 
-
-
-
-    
 pygame.quit()

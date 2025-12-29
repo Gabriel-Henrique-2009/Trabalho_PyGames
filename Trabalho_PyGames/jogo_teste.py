9f13cef1e7cdb6feebbba0c0e7290513c06b01c1
Eduardo23Ribeiro <eduardobr728@gmail.com>
2025-12-29 16:58:53 -0300

    arrumando os arquivos

--- Trabalho_PyGames/jogo_teste.py
+++ Trabalho_PyGames/jogo_teste.py
@@ -3,6 +3,8 @@ import pygame
 # att 1
 pygame.init()
 
+botao_menu_teste = pygame.Rect(500, 350, 200, 100)
+cor_botao_menu_teste = (255, 0, 0)
 tela = pygame.display.set_mode((1200, 800))
 rodando = True
 
@@ -10,11 +12,21 @@ while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+    if evento.type == pygame.MOUSEBUTTONDOWN:
+         if botao_menu_teste.collidepoint(evento.pos): # o collidepoint verifica se o mouse está sobre o botão.
+             #ele tbm evita ter que ficar fazendo conta da posição do mouse :) 
+             print("botão teste foi clicado iniciar o game")
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    print(mouse_x, mouse_y)
+    mouse_pos = pygame.mouse.get_pos()
+
+    if botao_menu_teste.collidepoint(mouse_pos):
+        cor_botao_menu_teste = (0, 255, 0)
+    else:
+        cor_botao_menu_teste = (255, 0, 0)
+
 
     tela.fill((0, 0, 0))
+    pygame.draw.rect(tela, cor_botao_menu_teste, botao_menu_teste)
     pygame.display.update()
 
     pygame.time.Clock().tick(60)
